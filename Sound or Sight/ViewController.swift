286eb5b704cdf8eddae80cd10a28e95bda6b34fc
bj-brooks <octatonic@gmail.com>
2024-12-27 20:46:03 -0600

    Initial Commit

---
+++ Sound or Sight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sound or Sight
+//
+//  Created by Bj Brooks on 12/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sound or SightTests/Sound_or_SightTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sound_or_SightTests.swift
+//  Sound or SightTests
+//
+//  Created by Bj Brooks on 12/27/24.
+//
+
+import Testing
+@testable import Sound_or_Sight
+
+struct Sound_or_SightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
